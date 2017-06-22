8b604dc31acc6eeb663e2ffd88991e279be2840d
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2017-06-22 18:18:35 -0300

    Added round and trunc

--- basic_arithmetic.exs
+++ basic_arithmetic.exs
@@ -1,5 +1,32 @@
+
+IO.puts "Operadores"
 IO.puts 1 + 2
 
 IO.puts 5 * 5
 
 IO.puts 10 / 2
+
+IO.puts div(10,2)
+
+IO.puts div 10, 2
+
+IO.puts rem 10, 3
+
+IO.puts "Binary - Octal"
+
+IO.puts 0b1010
+
+IO.puts 0o777
+
+IO.puts 0x1F
+
+IO.puts "Float numbers"
+
+IO.puts 1.0
+
+IO.puts 1.0e-10
+
+IO.puts "Float numbers for 64bits"
+
+IO.puts round(3.58)
+IO.puts trunc(3.58)
